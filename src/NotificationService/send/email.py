f6cf659984d2f8a299e0225a98789143dd843903
Adiga Thirthahalli Raghur <vadigathirthahall@deloitte.com>
2023-02-17 13:26:44 +0530

    Removed commented code from notification service

--- src/NotificationService/send/email.py
+++ src/NotificationService/send/email.py
@@ -1,24 +1,12 @@
-import smtplib, os, json
-from email.message import EmailMessage
+import json
 
 
 def notification(message):
     message = json.loads(message)
     mp3_fid = message["mp3_fid"]
-    # sender_address = os.environ.get("GMAIL_ADDRESS")
-    # sender_password = os.environ.get("GMAIL_PASSWORD")
     receiver_address = message["username"]
 
-    # msg = EmailMessage()
-    # msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
-    # msg["Subject"] = "MP3 Download"
-    # msg["From"] = sender_address
-    # msg["To"] = receiver_address
-    #
-    # session = smtplib.SMTP("smtp.gmail.com", 587)
-    # session.starttls()
-    # session.login(sender_address, sender_password)
-    # session.send_message(msg, sender_address, receiver_address)
-    # session.quit()
+    # Sending mail
+
     print("Mail Sent")
     print(f"User - {receiver_address} received mp3 id - {mp3_fid}")
